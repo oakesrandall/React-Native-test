c4d92ed1f555b884024529b3132ebdc6fdbbec1f
David Randall <oakesrandall@gmail.com>
2017-09-11 07:00:17 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to David's super complex React Native app!</Text>
+        <Text>Don't pretend that you're not impressed.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
